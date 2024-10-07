344f10740b1c99735322d8c9491ff7f9d5fe8367
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-07 17:59:19 +0530

    feat: add cors support

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,18 @@ import postRouter from "./post.js";
 import adminRouter from "./admin.js";
 import { authMiddleware } from "../middlewares/auth.js";
 import publicRouter from "./public/index.js";
+import cors from "cors";
+import "dotenv/config";
 
 const mainRouter = Router();
 
 mainRouter.use(express.urlencoded({ extended: true }));
 mainRouter.use(express.json());
+mainRouter.use(
+  cors({
+    origin: [process.env.VIEWER_FRONTEND_URL],
+  })
+);
 
 mainRouter.use("/", publicRouter);
 mainRouter.use("/auth", authRouter);
