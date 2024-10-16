eb59fddfd1eae5a05f9ccde59e2edd01695d1f25
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-16 12:52:06 +0530

    feat: return 401 when user is not found to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,7 +50,7 @@ const login = async (req, res) => {
       },
     });
     if (!user) {
-      res.status(404).send("User not found");
+      res.sendStatus(401);
       return;
     }
     const isCorrectPassword = await bcrypt.compare(password, user.password);
